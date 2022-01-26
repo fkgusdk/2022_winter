f4c0c6093ed3eb34e8e7562c963ea8e15e204bf7
RhaHyunAh <78009767+fkgusdk@users.noreply.github.com>
2022-01-26 15:56:42 +0900

    c언어 express chap 10

---
+++ chap 10/vac_p464_딱정벌레의 모험/vac_p464_딱정벌레의 모험/vac_p464_딱정벌레의 모험.cpp
@@ -0,0 +1,68 @@
+﻿
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    std::cout << "\n 페이지 464 딱정 벌레의 모험!\n\n";
+
+    int road[8][8]; // 길 표시할 테이블
+    for (int i = 0; i < 8; i++) { //테이블 초기화
+        for (int j = 0; j < 8; j++) {
+            road[i][j] = 0;
+        }
+    }
+
+    int row, col; //딱정벌레가 간 위치의 좌표
+    srand((double)time(NULL));
+    row = rand() % 8;
+    col = rand() % 8;
+
+    int move, num = 0; // 이동 방향 move (1~8), 해당 위치의 방문 순서 num
+    while (row < 8 and row >= 0 and col < 8 and col >= 0){
+        num++;
+        road[row][col] = num;
+
+        move = rand() % 8 + 1;
+
+        switch (move) {
+        case 1:
+            --col;
+            break;
+        case 2:
+            --col;
+            --row;
+            break;
+        case 3:
+            --row;
+            break;
+        case 4:
+            --row;
+            ++col;
+            break;
+        case 5:
+            ++col;
+            break;
+        case 6:
+            ++row;
+            ++col;
+            break;
+        case 7:
+            ++row;
+            break;
+        case 8:
+            ++row;
+            --col;
+            break;
+        }
+    }
+    for (int r = 0; r < 8; r++) { // 딱정벌레의 이동 출력
+        for (int c = 0; c < 8; c++) {
+            printf("%4d", road[r][c]);
+        }
+        printf("\n");
+    }
+}
+
+
---
+++ chap 10/vac_p465_find gold/vac_p465_find gold/vac_p465_find gold.cpp
@@ -0,0 +1,97 @@
+﻿
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int find_Gold(int x, int y, char m[10][10]) {
+
+    if (m[x][y] == 'G') {
+        printf("\n\n Congratulatons! You finally find Goooold !!! \n\n");
+        return 0;
+    }
+    else if (m[x][y] == 'M') {
+        printf("\n\n You're eaten by mine Monsters :( \n\n");
+        return 0;
+    }
+    else {
+        m[x][y] = 'Y';
+        printf("\n ============================ \n");
+        // 광산 구조 출력
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                printf("%2c", m[i][j]);
+            }
+            printf("\n");
+        }
+
+        // 이동하기
+        char move;
+        int originX = x, originY = y;
+        do {
+            x = originX; y = originY;
+            move = _getch();
+            switch (move) {
+            case 'b':
+                x--;
+                break;
+            case 'f':
+                x++;
+                break;
+            case 'l':
+                y--;
+                break;
+            case 'r':
+                y++;
+                break;
+            }
+            if (x < 0 or x > 9 or y < 0 or y > 9) {
+                printf("\n No path there. Try again!\n\n");
+            }
+            else break;
+        } while (true);
+        return find_Gold(x, y, m);
+    }
+}
+
+int main()
+{
+    std::cout << "\n < 몬스터를 피해 금을 찾자! >\n";
+
+    // 광산 기본 테이블
+    char mine[10][10];
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            mine[i][j] = '.';
+        }
+    }
+ 
+    srand((double)time(NULL)); //램덤 seed 초기화
+
+    // 광산에 몬스터 배정 (M)
+    int Mrow, Mcol, monster = 1;
+    while (monster < 16) {
+        Mrow = rand() % 10;
+        Mcol = rand() % 10;
+
+        if (mine[Mrow][Mcol] == 'M') continue;
+
+        mine[Mrow][Mcol] = 'M';
+        monster++;
+     }
+
+    //광산에 금 배정
+    int Grow, Gcol = 0;
+    do {
+        Grow = rand() % 10 +1;
+        Gcol = rand() % 10 +1;
+    } while (mine[Grow][Gcol] == 'M');
+    mine[Grow][Gcol] = 'G';
+
+    find_Gold(0, 0, mine);
+
+    return 0;
+}
+
+
+
