4eb807212670eb671266af8c6e6f894f036ca685
RhaHyunAh <78009767+fkgusdk@users.noreply.github.com>
2022-02-03 19:19:38 +0900

    c언어 express chap11-1

---
+++ vac_p488_포인터를 이용한 swap/vac_p488_포인터를 이용한 swap/vac_p488_포인터를 이용한 swap.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+void swap(int*, int*); // 매개변수도 포인터 변수형으로 넣어줘야 함
+int main()
+{
+    std::cout << "\n page 488 - 포인터를 이용한 swap (포인터 연습)\n\n";
+
+    int num1 = 10, num2 = 20;
+    printf(" swap 전 a,b : %d, %d \n", num1, num2);
+
+    swap(&num1, &num2); // 변수의 주소를 전달 !!!
+    printf(" swap 후 a,b : %d, %d \n", num1,num2);
+}
+
+void swap(int* a, int* b) { // 두 값을 바꿔주는 함수 (두 값의 주소를 받아 원본 값을 바꿀 수 있음)
+    int temp = *b; //temp 는  b에 담긴 주소에 담긴 값 저장
+    *b = *a; // b에 담긴 주소에 담긴 수를 *a로 교체
+    *a = temp; // a에 담긴 주소에 담긴 수를 temp로 교체
+
+    /*int temp = b;
+    * b = a;
+    * a = temp;
+    * 이렇게 하면 a와 b값만 바뀌는 것, num1과 num2의 주소가 바뀌는 것이 아님에 주의 !!!
+    */
+}
---
+++ vac_p490_slope/vac_p490_slope/vac_p490_slope.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+void slope(int, int, int, int, float *, float *);
+int main()
+{
+    std::cout << "\n page 490 기울기 예제 - 한 함수가 여러 값을 반환해야 할 때 포인터로 간접 반환해보기\n\n";
+
+    int x1, y1, x2, y2; // (x1,x2) , (x2,y2)
+
+    printf(" 점 1 의 좌표 : ");
+    scanf_s("%d %d", &x1, &y1);
+
+    printf(" 점 2 의 좌표 : ");
+    scanf_s("%d %d", &x2, &y2);
+
+    float s, yintercept; //기울기, y절편
+    slope(x1, y1, x2, y2, &s, &yintercept); // slope에서 반환 받고 싶은 값 s, yintercept는 주소를 보내주기
+
+    printf(" 기울기 :%f \n y절편 : %f", s, yintercept);
+}
+
+void slope(int x1, int y1, int x2, int y2, float *s, float *yintercept) { //s, yintercept의 주소를 저장해 해당 주소에 직접 저장 -> 반환 값 없지만 반환 효과 O
+    *s = (float)(y1 - y2) / (float)(x1 - x2);
+    *yintercept = y1 - (*s) * x1;
+}
+
