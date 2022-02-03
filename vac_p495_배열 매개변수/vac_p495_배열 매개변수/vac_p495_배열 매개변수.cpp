ea4a636a057e4d880f88059b9b9f88f8b3f9835c
RhaHyunAh <78009767+fkgusdk@users.noreply.github.com>
2022-02-03 19:25:36 +0900

    c언어 express chap 11-2

---
+++ vac_p495_배열 매개변수/vac_p495_배열 매개변수/vac_p495_배열 매개변수.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <stdio.h>
+#include <iostream>
+
+void change(int []);
+int main()
+{
+    std::cout << "\n page 495 - 배열 매개변수 연습 \n\n";
+
+    int a[5] = { 10,20,30,40,50 };
+
+    printf(" a = { ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("} \n\n");
+
+    change(a); //배열 주소 (배열 이름) 전달
+
+    printf(" 최종 결과 >>> \n\n");
+    printf(" a = { ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("} \n");
+}
+
+void change(int b[]) { // void func(int *b) 로 대체 가능
+    //이때 b는 배열을 가리키는 포인터로 동작
+    *b = 1;
+    printf(" *b = 1 실행 >> b[0] = %d\n", b[0]);
+
+    *(b + 1) = 2;
+    printf(" *(b+1) = 2 실행 >> b[1] =  %d\n", b[1]);
+
+    b[3] = 3;
+    printf(" b[3] = 3 실행 >> b[3] = %d\n\n", b[3]);
+}
+
---
+++ vac_p501_자울주행/vac_p501_자울주행/vac_p501_자울주행.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+void getSensorData(double*);
+int main()
+{
+    std::cout << "\n page 501 - 자율주행 자동차 예시 \n\n";
+
+    double sensorData[4]; //센서의 데이터를 저장할 배열
+    
+    getSensorData(sensorData); // 센서로 부터 데이터 받아오기 ( 함수로 배열의 이름, 즉 배열의 주소 전달 )
+
+    printf(" 좌측 장애물과의 거리 : %lf m \n", sensorData[0]);
+    printf(" 우측 장애물과의 거리 : %lf m \n", sensorData[1]);
+    printf(" 전방 장애물과의 거리 : %lf m \n", sensorData[2]);
+    printf(" 후방 장애물과의 거리 : %lf m \n\n", sensorData[3]);
+
+    for (int i = 0; i < 4; i++) { // 너무 가까우면 주의 문구
+        if (sensorData[i] < 2) printf(" 사고 위험 !!! 주의하세요 !!!\n\n");
+    }
+
+    return 0;
+}
+
+void getSensorData(double* p) { // 센서
+
+    srand((double)time(NULL)); //랜덤함수 seed 초기화
+
+    p[0] = rand()%100; // 좌
+    *(p + 1) = rand() % 100; // 우
+    p[2] = rand() % 100; // 전방
+    *(p + 3) = rand() % 100; // 후방
+}
